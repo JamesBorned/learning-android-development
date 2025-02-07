94a3cb265735a8065f977e8d213fb8bc4b12d698
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2025-02-07 21:01:38 +0300

    feat: check for correct data entry

--- app/src/main/java/com/fionova/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/fionova/myfirstapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.InputType
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private var selectedButton: androidx.appcompat.widget.AppCompatButton? = null
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         val button_number: androidx.appcompat.widget.AppCompatButton = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.button_number)
         val button_email: androidx.appcompat.widget.AppCompatButton = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.button_email)
         val input_email_number: EditText = findViewById<EditText>(R.id.input_email)
+        val button_sign_up: androidx.appcompat.widget.AppCompatButton = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.button_sign_up)
+        val input_password: EditText = findViewById<EditText>(R.id.input_password)
+        val input_password_again: EditText = findViewById<EditText>(R.id.input_password_again)
 
         selectedButton = button_number
 
@@ -29,6 +33,37 @@ class MainActivity : AppCompatActivity() {
             selectButton(button_email)
             input_email_number.hint = "Введите email"
             input_email_number.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        }
+
+        button_sign_up.setOnClickListener {
+            //Проверка email на содержание символа @
+            if (selectedButton === button_email) {
+                val email = input_email_number.text.toString()
+
+                if (!email.contains("@")) {
+                    Toast.makeText(this, "В email отсутствует знак @!", Toast.LENGTH_LONG).show()
+                }
+            }
+            //Проверка телефона на содержание символа +
+            if (selectedButton === button_number) {
+                val number = input_email_number.text.toString()
+
+                if (!number.contains("+")) {
+                    Toast.makeText(this, "В номере телефона отсутствует знак +!", Toast.LENGTH_LONG).show()
+                }
+            }
+            //Проверка пароля на количество символов (минимум 8)
+            val password = input_password.text.toString()
+
+            if (password.length < 8){
+                Toast.makeText(this, "Пароль содержит менее 8 символов!", Toast.LENGTH_LONG).show()
+            }
+            //Проверка, равны ли строки, взятые из пароля и его повторения
+            val password_again = input_password_again.text.toString()
+
+            if (password_again.compareTo(password) !== 0){
+                Toast.makeText(this, "Пароль неверный!", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
